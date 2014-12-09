b3d5e315cf469bfecb840cf749beb8d7446f2ff6
Austin Davis-Richardson <harekrishna@gmail.com>
2014-12-09 14:02:28 -0500

    initial commit

---
+++ irc-bot.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+
+
+ping <- function() { "PONG :ingis\n" }
+
+sendmsg <- function(chan, msg) { paste('PRIVMSG', chan, ':', msg, "\n") }
+
+joinchan <- function(chan) { paste('JOIN', chan, "\n") }
+
+connect <- function() {
+  socket <- make.socket(host = 'localhost', 6667, fail = TRUE, server = FALSE)
+
+  write.socket(socket, "USER rbot rbot rbot :i am a bot\n")
+  write.socket(socket, "NICK robt \n")
+
+  write.socket(socket, joinchan('#test'))
+
+  listen(socket)
+}
+
+listen <- function(socket) {
+  repeat {
+    ircmsg <- read.socket(socket, loop = FALSE)
+    print(ircmsg)
+  }
+}
+
+connect()
