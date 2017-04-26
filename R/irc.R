dbf6600de90d88a56f3a5fcb2d113ee8a9ebc269
Austin Davis-Richardson <harekrishna@gmail.com>
2017-04-25 22:16:30 -0700

    fixes nickname

--- R/irc.R
+++ R/irc.R
@@ -8,11 +8,12 @@ connect <- function(host, port, nickname) {
   socket <- make.socket(host = host, port, fail = TRUE, server = FALSE)
 
   write.socket(socket, sprintf("USER %s %s %s :%s\n", nickname, nickname, nickname, "r-bot"))
-  write.socket(socket, "NICK robt \n")
+  write.socket(socket, sprintf("NICK %s \n", nickname))
 
   write.socket(socket, joinchan('#test'))
 
   listen(socket)
+  # TODO: somehow handle line protocol here ...
 }
 
 listen <- function(socket) {
